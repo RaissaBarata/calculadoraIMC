74ec2ed509c39b176004c7810a10cafbf8a46026
Raissa <barata.raissa@gmail.com>
2022-12-07 16:11:31 -0300

    Algumas alterações (simplificando o código)

--- imc.js
+++ imc.js
@@ -1,13 +1,12 @@
-const calcular = document.getElementById ('calcular');
-
+const calcular = document.getElementById('calcular');
 
 function imc () {
     const nome = document.getElementById('nome').value;
     const altura = document.getElementById('altura').value;
     const peso = document.getElementById('peso').value;
-    const resultado = document.getElementById ('resultado');
+    const resultado = document.getElementById('resultado');
 
-    if (nome !== '' && altura !== '' && peso !== '') {
+    if (nome && altura  && peso) {
        const valorIMC = (peso / (altura * altura)).toFixed(1);
 
        let classificacao = '';
